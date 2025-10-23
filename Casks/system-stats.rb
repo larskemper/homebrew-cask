c2654538ade16dc68391794e3de9ec80c9c342f0
Lars Kemper <l.kemper@shopware.com>
2025-10-23 18:35:34 +0100

    feat: update checksums

--- Casks/system-stats.rb
+++ Casks/system-stats.rb
@@ -1,25 +1,23 @@
 cask "system-stats" do
   version "0.1.0"
-  
   arch arm: "aarch64", intel: "x86_64"
-  
+
+  sha256 arm:   "82ac20a655856cc1501853c5a7de7d9435ef2a4085ef94c227985ba7919cb847",
+         intel: "f9bc57d9bf8a096ee81e1fb3310ea85409093e3eac0f073829e977f58b75fecd"
+
   url "https://github.com/larskemper/macos-menu-bar-stats/releases/download/v#{version}/system-stats-#{arch}.dmg"
-  sha256 arm:   "f9bc57d9bf8a096ee81e1fb3310ea85409093e3eac0f073829e977f58b75fecd",
-         intel: "1e68934f3a6c4c549c52e7daf2670dd6132c13ce9b43a8d7f0561fe7bda58f40"
-  
   name "System Stats"
-  desc "Simple macOS menu bar app for system monitoring"
+  desc "Simple menu bar app for system monitoring"
   homepage "https://github.com/larskemper/macos-menu-bar-stats"
 
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :ventura"
 
-  app "System stats.app"
+  app "System Stats.app"
 
   zap trash: [
-    "~/Library/Application Support/dev.larskemper.system-stats",
-    "~/Library/Caches/dev.larskemper.system-stats",
     "~/Library/Preferences/dev.larskemper.system-stats.plist",
+    "~/Library/Application Support/System Stats",
+    "~/Library/Logs/System Stats",
     "~/Library/Saved Application State/dev.larskemper.system-stats.savedState",
   ]
 end
-
