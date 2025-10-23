d923ae75b0a455e79a48e76f58f6f6e68873e890
Lars Kemper <l.kemper@shopware.com>
2025-10-23 19:11:54 +0100

    feat: update checksums

--- Casks/system-stats.rb
+++ Casks/system-stats.rb
@@ -2,8 +2,8 @@ cask "system-stats" do
   version "0.1.0"
   arch arm: "aarch64", intel: "x86_64"
 
-  sha256 arm:   "82ac20a655856cc1501853c5a7de7d9435ef2a4085ef94c227985ba7919cb847",
-         intel: "f9bc57d9bf8a096ee81e1fb3310ea85409093e3eac0f073829e977f58b75fecd"
+  sha256 arm:   "6b6b83b92bac7efbfdcbd84635687970dd44e10a93573931a9cdd8696491f34e",
+         intel: "8e488330c61ca2cf48781699453079291e8a3e3d1c74a4dd8fcf9852ab3bdadb"
 
   url "https://github.com/larskemper/macos-menu-bar-stats/releases/download/v#{version}/system-stats-#{arch}.dmg"
   name "System Stats"
